a4a1c6fdcad06ae4fa9ed4ed183ae113d71ac373
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-09-06 15:29:43 +1000

    changed the token creation logic

--- python/aristotle-mdr-api/aristotle_mdr_api/management/commands/create_access_token.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/management/commands/create_access_token.py
@@ -8,11 +8,12 @@ from django.core.management.base import BaseCommand
 from django.test import override_settings
 
 from django.test import TestCase, Client, override_settings, tag
-# from aristotle_mdr_api.token_auth.models import AristotleToken
+from aristotle_mdr_api.token_auth.models import AristotleToken
 import json
 from django.urls import reverse
 import static_precompiler
 from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
     args = ''
@@ -20,11 +21,6 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
-        # parser.add_argument(
-        #     "--noinput",
-        #     default=False,
-        #     action='store_true',
-        # )
 
     def handle(self, *args, **options):
         all_true_perms =  {
@@ -43,11 +39,13 @@ class Command(BaseCommand):
             }
         }
 
-        client = Client()
-        client.post(reverse('logout'), {})
-        response = client.post(reverse('friendly_login'), {'username': 'admin@myproject.com', 'password': 'password'})
-        postdata = {'name': 'div', 'perm_json': json.dumps(all_true_perms)}
-        response = client.post('/api/token/create/', postdata)
-        return response.context_data['key']
+        user = get_user_model().objects.get(email='admin@myproject.com')
+        token = AristotleToken.objects.create(
+            name='name',
+            user=user,
+            permissions=all_true_perms
+        )
+        return token.key
+        
 
 
