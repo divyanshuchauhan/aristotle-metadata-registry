bc165f2c8e88660ed29b78127c69055eaec4725f
Divyanshu <divyanshuchauhan0208@gmail.com>
2018-09-06 13:12:23 +1000

    modified the url for api token create in create_access_token4

--- python/aristotle-mdr-api/aristotle_mdr_api/management/commands/create_access_token.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/management/commands/create_access_token.py
@@ -47,5 +47,5 @@ class Command(BaseCommand):
         client.post(reverse('logout'), {})
         response = client.post(reverse('friendly_login'), {'username': 'admin@myproject.com', 'password': 'password'})
         postdata = {'name': 'div', 'perm_json': json.dumps(all_true_perms)}
-        response = client.post('/api/token/create/', postdata)
+        response = client.post('token_auth:token_create', postdata)
         return response.context_data['key']
